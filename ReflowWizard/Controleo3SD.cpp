d05582a440d5c1e149d1fb83a5ecee827ea4b1ac
Steven Johnson <sakurainds@gmail.com>
2018-05-14 21:58:32 +0800

    spelling correction

--- ReflowWizard/Controleo3SD.cpp
+++ ReflowWizard/Controleo3SD.cpp
@@ -450,7 +450,7 @@ File SDClass::open(const char *filepath, uint8_t mode) {
 
   int pathidx;
 
-  // do the interative search
+  // do the interactive search
   SdFile parentdir = getParentDir(filepath, &pathidx);
   // no more subdirs!
 
