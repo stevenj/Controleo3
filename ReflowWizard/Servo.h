022053addc334ebf113bebddda839da4384576e9
Steven Johnson <sakurainds@gmail.com>
2018-05-16 22:25:59 +0800

    rename Servo.ino to be a C++ Module Add a Servo.h to match

---
+++ ReflowWizard/Servo.h
@@ -0,0 +1,18 @@
+#ifndef __SERVO_H__
+#define __SERVO_H__
+
+#include <Arduino.h>
+
+// Starts the timer.  Called on startup
+void initializeTimer(void);
+
+// Interrupt handler for TC3
+void TC3_Handler(void);
+
+// Move the servo to servoDegrees, in timeToTake milliseconds (1/1000 second)
+void setServoPosition(uint8_t servoDegrees, uint16_t timeToTake);
+
+// Convert degrees (0-180) to a timer counter value
+uint16_t degreesToTimerCounter(uint8_t servoDegrees);
+
+#endif
