aaaf6e0a17498ec6019c0b206d0c48bcd122474b
Steven Johnson <sakurainds@gmail.com>
2018-06-17 12:13:16 +0800

    Use global clock rate constant

--- OvenACE/piezo_buzzer.h
+++ OvenACE/piezo_buzzer.h
@@ -4,12 +4,13 @@
 #ifndef __PIEZO_BUZZER__H__
 #define __PIEZO_BUZZER__H__
 
+#include "HWPinAssignments.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
-// TODO: Make the Clk Constant come from a central location.
-#define HZ_TO_FREQ(hz) (1800000/hz)
+#define HZ_TO_FREQ(hz) (GCLK3_HZ/hz)
 
 // Flash a particular debug led, a particular number of times.
 void PlayTone(uint16_t freq);
