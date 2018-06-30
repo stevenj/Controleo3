63a6a31f54badcd22c95cbecfe1099f09dd7d831
Steven Johnson <sakurainds@gmail.com>
2018-06-30 22:55:19 +0800

    Add Bitbashed SDCard SPI Write Function

--- OvenACE/BitBash.h
+++ OvenACE/BitBash.h
@@ -1,8 +1,18 @@
 #ifndef __BITBASH_H__
 #define __BITBASH_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 #include <stdint.h>
 
-extern "C" uint8_t TC_SPI_ONE(uint8_t tx);
+uint8_t TC_SPI_ONE(uint8_t tx);
+
+void TX_SDCARD(uint8_t byte);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 #endif
